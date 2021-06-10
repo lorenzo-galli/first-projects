05203f5a2bae1b9f6e80509be955f5108bc547be
Lorenzo Galli <galli.lorenzo.x@gmail.com>
2021-06-10 10:24:30 +0200

    Uploading first files

---
+++ Factorization.py
@@ -0,0 +1,14 @@
+number = input("Insert a number to factor: ")
+
+f = open("primes.txt", 'a')
+primes = f.readlines()
+
+
+def is_prime(num):
+    pass
+
+for prime in primes:
+    pass
+
+def write_number(num):
+    pass
