728d97d27058c949e6923548be897bb645fe53c6
Lorenzo Galli <galli.lorenzo.x@gmail.com>
2021-06-10 15:27:42 +0200

    Creating Factorizer function

--- factoring/Factorization.py
+++ factoring/Factorization.py
@@ -1,15 +1,26 @@
 number = int(input("Insert a number to factor: "))
-print(10+number)
-f = open("primes.txt", 'a')
-primes = f.readlines()
 
-print(primes)
+num_divisors = []
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 39, 41, 43, 47, 53]
+original_num = number
+
+def factorizer(num):
+    i = 0
+    for prime in primes:
+        while num != 1:
+            if num % primes[i] == 0:
+                num = num/primes[i]
+                num_divisors.append(primes[i])
+            i += 1
+    print(num_divisors)
+
+factorizer(number)
 
 def is_prime(num):
-    pass
+   pass
 
 for prime in primes:
     pass
 
-def write_number(num):
+def write_num(num):
     pass
