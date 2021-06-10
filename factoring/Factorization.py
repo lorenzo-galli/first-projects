1a9c10ca69acdad7d275b2cbfb78e747520b9f9e
Lorenzo Galli <galli.lorenzo.x@gmail.com>
2021-06-10 22:40:13 +0200

    Add the primes to the .txt file and add comments

--- factoring/Factorization.py
+++ factoring/Factorization.py
@@ -1,47 +1,65 @@
+# A factorizator developed by Lorenzo Galli 
+
+print('Enter a number and this project will factorize it for you.')
+print('Numbers that have factors larger than 7919 (999th prime) won\'t be factorized')
 number = int(input("Insert a number to factor: "))
 
+# Declare variables and initialize lists and dictionaries
 num_divisors = []
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 39, 41, 43, 47, 53]
 original_num = number
 exp_dict = {}
 factors = "1"
 
+# Open the file with the primes, reads it and creates a list 
+with open('primes.txt', 'r') as f:
+    primes =[]
+    for line in f:
+        line = line.strip()
+        line = int(line)
+        primes.append(line)
 
+# This is the main function, it takes as input the user number
 def factorizer(num):
-    i = 0
+    # Add a index to select the prime
+    i = 0    
     for prime in primes:
+        # If num = 1 it means we succesfully factorized it also i cannot
+        # be bigger than the lenght of the list or it will throw an error
         while num != 1 and i < primes.__len__():
+            # If the number is divisible it adds it to the divisors list
             if num % primes[i] == 0:
                 num = num/primes[i]
                 num_divisors.append(primes[i])
+            # If not it just changes prime and repeat the cicle
             else:
                 i += 1
+        # Some primes might be repeated so we use exponents
         exponent = num_divisors.count(prime)
         exp_dict[prime] = exponent
 
 
-def is_prime():
-    if num_divisors.__len__() == 0:
+# This function prints if the number is prime and its factors 
+def print_num():
+    if num_divisors.__len__() == 1:
         print("Is prime")
-        primes.append(number)
-        print(primes)
+    else:
+        print("It's not prime")
+    print(str(number) + " = " + str(factors))
 
 
 factorizer(number)
 
-
+# This loop creates the final string with the factors
 for prime in exp_dict:
+    # If it isn't a factor it doesn't display it
     if exp_dict[prime] == 0:
         pass
+    # If it is it displays a * with the factors
     else:
         factors += " * " + str(prime) 
+        # If it has an exp it displays it with ^
         if exp_dict[prime] != 1:
             factors += "^" + str(exp_dict[prime])
 
 
-def print_num():
-    print(str(number) + " = " + str(factors))
-
-
-is_prime()
 print_num()
