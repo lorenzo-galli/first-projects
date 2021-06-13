7c1e853a1624b4b1a47d2983692614151fcb3fb7
Lorenzo Galli <galli.lorenzo.x@gmail.com>
2021-06-13 21:48:36 +0200

    Screw some things up

--- sorters/quick_sort.py
+++ sorters/quick_sort.py
@@ -1,49 +1,65 @@
 from math import pi
 import random
 with open('random_numbers.txt', 'r') as f:
-    unsorted = []
-    for line in f:
+    unsorted = []  # initialize an empty array
+    for line in f: # and fill it with the numbers from the text file
         line = line.strip()
         line = int(line)
         unsorted.append(line)
 
+def is_sorted(to_sort):
+    o = 0  # use this condition to make sure it doesn't go out of index
+    while o < to_sort.__len__() - 1:
+        if to_sort[o] > to_sort[o + 1]:  # if one element is bigger than the next
+            return False  # the array is not sorted
+        else:
+            o += 1
+    return True  # if it loops in the whole array it means it's sorted
 
 def quicksort(to_sort): 
-    list_len = to_sort.__len__()
-    i_pivot = random.randint(0, list_len - 1)
-    pivot = to_sort[i_pivot]
-    i = 0
-    print(pivot)
-    while i < list_len:
-        if to_sort[i] > pivot and i < i_pivot:
-            to_sort.append(to_sort[i])
-            to_sort.pop(i)
-            i_pivot -= 1
-        elif to_sort[i] < pivot and i > i_pivot:
-            to_sort.insert(0, to_sort[i])
-            to_sort.pop(i + 1)
-            i += 1
-        elif to_sort[i] == pivot:
-            i += 1
+    if len(to_sort) == 1 or is_sorted(to_sort) is True:
+        print("There ain't nothing to sort XD")
+        return to_sort
+    else:
+        i_pivot = random.randint(0, to_sort.__len__() - 1)
+        pivot = to_sort[i_pivot]
+        i = 0
+        print(pivot)
+        while i < len(to_sort):
+            if to_sort[i] > pivot and i < i_pivot:
+                to_sort.append(to_sort[i])
+                to_sort.pop(i)
+                i_pivot -= 1
+            elif to_sort[i] < pivot and i > i_pivot:
+                to_sort.insert(0, to_sort[i])
+                to_sort.pop(i + 1)
+                i += 1
+            elif to_sort[i] == pivot:
+                i += 1
+            else:
+                i += 1
+        
+        i_pivot = to_sort.index(pivot)
+        sort1 = to_sort[0:i_pivot]
+        sort2 = to_sort[i_pivot + 1:len(to_sort)]
+        if is_sorted(to_sort):
+            pivot_list = [pivot]
+            to_sort = sort1 + pivot_list + sort2
+            print(to_sort)
+            return to_sort
         else:
-            i += 1
-    i_pivot = to_sort.index(pivot)
-    sort1 = to_sort[0:i_pivot]
-    sort2 = to_sort[i_pivot + 1:list_len]
-    return sort1, sort2
+            print(to_sort)
+            quicksort(sort1)
+            quicksort(sort2)
+            return sort1, sort2
 
-def is_sorted(to_sort):
-    list_len = to_sort.__len__()
-    o = 0
-    while o < list_len - 1:
-        if to_sort[o] > to_sort[o + 1]:
-            return False
-        elif to_sort[o] == to_sort[o + 1]:
-            o += 1
-        else:
-            o += 1
-    return True
 
+  #  if len(unsorted) < 2:
+ #       print("There ain't nothing to sort XD")
+  #  else:
+  #      quicksort(unsorted)
+  
 print(unsorted)
-print(quicksort(unsorted))
+quicksort(unsorted)
+print(unsorted)
 print(is_sorted(unsorted))
